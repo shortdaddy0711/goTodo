0291c8a5d667ef7f7f438627568b9cd2c3c9a1ad
shortdaddy0711 <sodoatm@gmail.com>
2021-03-17 17:15:04 -0700

    refactored code to add DB

--- app/app.go
+++ app/app.go
@@ -3,48 +3,34 @@ package app
 import (
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/shortdaddy0711/goTodo/model"
 	"github.com/unrolled/render"
 )
 
 var rd *render.Render
-
-type Todo struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	Completed bool      `json:"completed"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
-var todoMap map[int]*Todo
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/todo.html", http.StatusTemporaryRedirect)
 }
 
 func getTodoListHandler(w http.ResponseWriter, r *http.Request) {
-	list := []*Todo{}
-	for _, v := range todoMap {
-		list = append(list, v)
-	}
+	list := model.GetTodos()
 	rd.JSON(w, http.StatusOK, list)
 }
 
 func addTodoHandler(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
-	id := len(todoMap) + 1
-	todo := &Todo{id, name, false, time.Now()}
-	todoMap[id] = todo
+	todo := model.AddTodo(name)
 	rd.JSON(w, http.StatusCreated, todo)
 }
 
 func removeTodoHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	intId, _ := strconv.Atoi(vars["id"])
-	if _, ok := todoMap[intId]; ok {
-		delete(todoMap, intId)
+	ok := model.RemoveTodo(intId)
+	if ok {
 		rd.JSON(w, http.StatusOK, true)
 	} else {
 		rd.JSON(w, http.StatusBadRequest, false)
@@ -55,8 +41,8 @@ func completeTodoHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	intId, _ := strconv.Atoi(vars["id"])
 	complete := r.FormValue("complete") == "true"
-	if todo, ok := todoMap[intId]; ok {
-		todo.Completed = complete
+	ok := model.CompleteTodo(intId, complete)
+	if ok {
 		rd.JSON(w, http.StatusOK, true)
 	} else {
 		rd.JSON(w, http.StatusBadRequest, false)
@@ -64,7 +50,6 @@ func completeTodoHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func MakeHandler() http.Handler {
-	todoMap = make(map[int]*Todo)
 
 	rd = render.New()
 	r := mux.NewRouter()
--- app/app_test.go
+++ app/app_test.go
@@ -2,11 +2,14 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strconv"
 	"testing"
 
+	"github.com/shortdaddy0711/goTodo/model"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,7 +23,7 @@ func TestTodos(t *testing.T) {
 	assert.NoError(err)
 	defer resp.Body.Close()
 	assert.Equal(http.StatusCreated, resp.StatusCode)
-	var todo Todo
+	var todo model.Todo
 	err = json.NewDecoder(resp.Body).Decode(&todo)
 	assert.NoError(err)
 	assert.Equal(v1["name"][0], todo.Name)
@@ -37,11 +40,53 @@ func TestTodos(t *testing.T) {
 	assert.Equal(v2["name"][0], todo.Name)
 	id2 := todo.ID
 
-	resp, err = http.Get(ts.URL+"/todos")
+	resp, err = http.Get(ts.URL + "/todos")
 	assert.NoError(err)
-	assert.Equal(http.StatusCreated, resp.StatusCode)
-	todos := []*Todo{}
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	todos := []*model.Todo{}
 	err = json.NewDecoder(resp.Body).Decode(&todos)
 	assert.NoError(err)
 	assert.Equal(len(todos), 2)
+	for _, td := range todos {
+		if td.ID == id1 {
+			assert.Equal("Test todo1", td.Name)
+		}
+		if td.ID == id2 {
+			assert.Equal("Test todo2", td.Name)
+		} else {
+			assert.Error(fmt.Errorf("testID should be id1 or id2"))
+		}
+	}
+
+	resp, err = http.Get(ts.URL + "/complete/" + strconv.Itoa(id1) + "?complete=true")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	resp, err = http.Get(ts.URL + "/todos")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	todos = []*model.Todo{}
+	err = json.NewDecoder(resp.Body).Decode(&todos)
+	assert.NoError(err)
+	assert.Equal(len(todos), 2)
+	for _, td := range todos {
+		if td.ID == id1 {
+			assert.True(td.Completed)
+		}
+	}
+
+	req, _ := http.NewRequest("DELETE", ts.URL+"/todos/"+strconv.Itoa(id1), nil)
+	resp, err = http.DefaultClient.Do(req)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	resp, err = http.Get(ts.URL + "/todos")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	todos = []*model.Todo{}
+	err = json.NewDecoder(resp.Body).Decode(&todos)
+	assert.NoError(err)
+	assert.Equal(len(todos), 1)
+	for _, td := range todos {
+		assert.Equal(td.ID, id2)
+	}
+
 }
